947046fd21f6597dd87d59ac0430dd25612f4b1e
Tomer Shani <tomershani2008@gmail.com>
2019-11-21 22:27:49 +0200

    build path map with several help-functions, also added debuging print-map function

--- permutation_Array/permutation_Array.cpp
+++ permutation_Array/permutation_Array.cpp
@@ -23,6 +23,45 @@ void writeInputToArray(int* arr, int length)
 	}
 }
 
+int lengthOfPath(int num, const int* arr)
+{
+	int count = 1;
+	int index = num;
+	while (arr[index] != num)
+	{
+		index = arr[index];
+		++count;
+	}
+	return count;
+}
+
+void setPath(int num, int* base, const int* arr)
+{
+	int index = num;
+	int count = 0;
+	while (arr[index] != num)
+	{
+		base[count] = arr[index];
+		index = arr[index];
+		++count;
+	}
+	base[count] = arr[index];
+}
+
+// print function for debugging and inspecting
+void printMap(int** map, const int* permArr, int length)
+{
+	for (int i = 0; i < length; ++i)
+	{
+		std::cout << "Path of " << i << ": ";
+		for (int j = 0; j < lengthOfPath(i, permArr); ++j)
+		{
+			std::cout << map[i][j] << " ";
+		}
+		std::cout << '\n';
+	}
+}
+
 int main()
 {
 	//get array size from user (with input check)
@@ -35,14 +74,23 @@ int main()
 	//alocate dynamic array
 	int *permutationArray = new int[arraySize];
 	
-	//get values for array (with input check)
+	//get values for array
 	writeInputToArray(permutationArray, arraySize);
+	//input inspection of the array
+	
+	//build path map
+	int** map = new int* [arraySize];
+	for (int i = 0; i < arraySize; ++i)
+	{
+		map[i] = new int[lengthOfPath(0, permutationArray)];
+		setPath(i, map[i], permutationArray);
+	}
+	printMap(map, permutationArray, arraySize);
 
-	//build actual array
-	//build road map
 	//get number to check
-	//print road to number
+	//print path to number
 	//special cases:
-		//if -1 print all roads and exit
+		//if -1 print all paths and exit
 		//if anything else print error and exit
+
 }
